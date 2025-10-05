5e7e5ee874241803b10163cfdb5e06550b879d97
Isiahosborn <iosborn@fingerlakes.edu>
2025-10-05 19:16:12 -0400

    Update compliment_01.py

--- compliment_01.py
+++ compliment_01.py
@@ -12,7 +12,7 @@
 response = input()
 if response == "yes":
     print("You have wonderful eyes.")
-    print("Thank you for playing.")
+print("Thank you for playing.")
 else:
     print("Thank you for playing.")
 
